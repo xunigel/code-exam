8b77d89d755f25284ee3a43cdfa4191957fafb43
Nigel Xu <nigelxu@NigeldeMacBook-Pro.local>
2024-12-09 22:07:41 +0800

    init java project

---
+++ src/main/java/hsbc/interview/App.java
@@ -0,0 +1,13 @@
+package hsbc.interview;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! ready4interview" );
+    }
+}
